45939532ba615f4b088fc229b40b3480fb62ec16
Steven Merrill <steven.merrill@gmail.com>
2015-11-06 12:11:26 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <?php
 
-echo "Welcome to the OpenShift 3 Roadshow Smoke Test Application";
+header('Content-Type: text/plain');
+
+echo "Welcome to the OpenShift 3 Roadshow Smoke Test Application.\n\n";
+
+echo "Check out some awesome server stuff.\n\n"
 print_r($_SERVER);
 
